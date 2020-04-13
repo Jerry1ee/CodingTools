35e9c80923ccdeed358c3aa4b5dff9a68814d730
Jerry1ee <34805730+552000264@users.noreply.github.com>
2020-04-13 12:08:56 +0800

    Initial commit

---
+++ src/main/java/com/lzy/codingtools/CodingtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.lzy.codingtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingtoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzy/codingtools/CodingtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzy.codingtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingtoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
